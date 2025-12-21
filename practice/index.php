48f8457cf01928befec2e5c3c54b871b9a956902
alexey2hu <alexeybyr.code@gmail.com>
2025-12-21 13:11:37 +0300

    2h

--- practice/index.php
+++ practice/index.php
@@ -5,42 +5,27 @@
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon" />
-	<title>HTML 5</title>
+	<link rel="shortcut icon" href="./images/icons/favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="./styles/index.css">
+	<title>HTML5+CSS3</title>
 </head>
 
 <body>
-	<!-- <form>
-		<label for="#inputTextID">Подсказка: </label>
-		<input type="text" id='inputTextID'>
-		<select name="select">
-			<option value="default">-- По умолчанию --</option>
-			<option value="1">-- Пункт 1 --</option>
-			<option value="three">-- Пункт 2 --</option>
-		</select>
-		<textarea name="" id="" cols="20" rows="10"></textarea>
-		<button type='submit'>Отправить данные</button>
-	</form> -->
+	<!-- Абсолютные пути-->
+	<a href="https://google.com">Ссылка на гугл</a>
+	<img src="d:/...">
 
-	<form action="">
-		<!-- Добавляет границу для данных внутри -->
-		<fieldset>
-			<legend>Два инпута</legend>
-			<input type="text">
-			<input type="text">
-		</fieldset>
-		<fieldset>
-			<legend>Инпут + выбор</legend>
-			<input type="text">
-			<select name="select">
-				<option value="0">-- По умолчанию --</option>
-				<option value="1">-- Пункт 1 --</option>
-				<option value="2">-- Пункт 2 --</option>
-			</select>
-		</fieldset>
-		<button type='submit'>submit</button>
-		<button type='reset'>reset</button>
-	</form>
+	<!-- Относительные пути-->
+	<!-- С текущей папки -->
+	<img src="images/icons/favicon.ico">
+	<!-- С текущей папки (Явное уточнение) -->
+	<img src="./images/icons/favicon.ico">
+	<!-- Выход с текущей папки (../) -->
+	<hr>
+	<img src="../practice/images/icons/favicon.ico">
+	<br>
+	<!-- От корня проекта (!Лучший вариант для серверов!) -->
+	<img src="/practice/images/icons/favicon.ico">
 </body>
 
 </html>
