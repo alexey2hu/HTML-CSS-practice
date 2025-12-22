8352c1a3ebe406efc3c591f13f166c1cd1a4d4ca
alexey2hu <alexeybyr.code@gmail.com>
2025-12-22 17:12:48 +0300

    2.5h

--- practice/index.php
+++ practice/index.php
@@ -11,21 +11,69 @@
 </head>
 
 <body>
-	<!-- Абсолютные пути-->
-	<a href="https://google.com">Ссылка на гугл</a>
-	<img src="d:/...">
+	<!-- БЛОК -->
+	<div class="user-card">
+		<!-- БЛОК__ЭЛЕМЕНТ -->
+		<img class="user-card__image"></img>
+		<h3 class="user-card__user-nick">Nick</h3>
+		<!-- БЛОК__ЭЛЕМЕНТ БЛОК__ЭЛЕМЕНТ--МОДИФИКАТОР -->
+		<p class="user-card__desc user-card__desc--color-green">
+		</p>
+	</div>
 
-	<!-- Относительные пути-->
-	<!-- С текущей папки -->
-	<img src="images/icons/favicon.ico">
-	<!-- С текущей папки (Явное уточнение) -->
-	<img src="./images/icons/favicon.ico">
-	<!-- Выход с текущей папки (../) -->
-	<hr>
-	<img src="../practice/images/icons/favicon.ico">
-	<br>
-	<!-- От корня проекта (!Лучший вариант для серверов!) -->
-	<img src="/practice/images/icons/favicon.ico">
+	<div class="greet-block">
+		<div class="greet-block__content">
+			<div class="greet-title"></div>
+			<div class="greet-button"></div>
+		</div>
+		<div class="greet-block__video"></div>
+	</div>
+	<div class="steps-block">
+		<div class="steps-block__step">
+			<div class="steps-block__step-title"></div>
+			<div class="steps-block__step-desc"></div>
+		</div>
+		<div class="steps-block__step">
+			<div class="steps-block__step-title"></div>
+			<div class="steps-block__step-desc"></div>
+		</div>
+		<div class="steps-block__step">
+			<div class="steps-block__step-title"></div>
+			<div class="steps-block__step-desc"></div>
+		</div>
+		<div class="steps-block__step">
+			<div class="steps-block__step-title"></div>
+			<div class="steps-block__step-desc"></div>
+		</div>
+	</div>
+	<div class="about">
+		<div class="about__row">
+			<div class="about__content">
+				<div class="about__title"></div>
+				<div class="about__text"></div>
+				<div class="about__button"></div>
+			</div>
+			<div class="about__video"></div>
+		</div>
+		<div class="about__row">
+			<div class="about__images">
+				<div class="about__images-item--main"></div>
+				<div class="about__images-item"></div>
+				<div class="about__images-item"></div>
+			</div>
+			<div class="about__content">
+				<div class="about__title"></div>
+				<div class="about__text"></div>
+				<div class="about__button"></div>
+				<div class="social">
+					<div class="social__item"></div>
+					<div class="social__item"></div>
+					<div class="social__item"></div>
+					<div class="social__item"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
